d3fc3fa4bd7e5b84a816bd4c5eae522bab62577f
Arshit <arshitbaliyan123@gmail.com>
2026-08-07 16:11:24 +0530

    still handling mail

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,10 +1,8 @@
 const nodemailer = require('nodemailer');
 
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  family: 4, // Force IPv4
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
